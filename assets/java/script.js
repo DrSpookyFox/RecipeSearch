020ce47ac8e43668615af1cb557f9c0ff7239eed
Ed Kubiak <ek33450505@gmail.com>
2022-03-21 16:59:25 -0400

    corrected function

--- assets/java/script.js
+++ assets/java/script.js
@@ -4,16 +4,16 @@
 var apiKey = "76fc45feadbe46379e4c23a107066a2f";
 
 //function calls spooacular API to get recipe info
-var getRandomRecipe = function(id) {
+var searchRecipies = function(data) {
     // format the github api url
-    var apiUrl = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`
+    var apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`
       
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(id) {
-        console.log(id);
+        response.json().then(function(data) {
+        console.log(data);
         });
       });
     };
-    getRandomRecipe();
+    searchRecipies();
   
