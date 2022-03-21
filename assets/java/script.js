3dd16f7c290b8bc279a796713255931f3e200a7f
Ed Kubiak <ek33450505@gmail.com>
2022-03-21 16:57:23 -0400

    reworking homepage for search

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,8 +1,19 @@
+
+
 //Global Variables
 var apiKey = "76fc45feadbe46379e4c23a107066a2f";
 
 //function calls spooacular API to get recipe info
-var getRecipeInfo = function() {
-  fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`);
-};
-getRecipeInfo();
+var getRandomRecipe = function(id) {
+    // format the github api url
+    var apiUrl = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`
+      
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(id) {
+        console.log(id);
+        });
+      });
+    };
+    getRandomRecipe();
+  
