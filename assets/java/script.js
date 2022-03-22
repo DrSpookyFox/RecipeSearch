8ec02b40ec918794445e6209c2792f1f90cd9e6c
Ed Kubiak <ek33450505@gmail.com>
2022-03-21 20:36:18 -0400

    added formhandler

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,7 +1,22 @@
-
+var userFormEl = document.querySelector("#search-form");
+var nameInputEl = document.querySelector("#recipeName");
 
 //Global Variables
 var apiKey = "76fc45feadbe46379e4c23a107066a2f";
+
+
+var formSumbitHandler = function(event) {
+    //Prevents browser from sending the form's input data to a URL
+    event.preventDefault();
+    // get  value from input element
+    var recipeName = nameInputEl.value.trim();
+
+    if (recipeName) {
+        searchRecipies(recipeName);
+        nameInputEl.value = "";
+        } else 
+        alert("Please try again: not a valid search option!")
+    }
 
 //function calls spooacular API to get recipe info
 var searchRecipies = function(data) {
@@ -10,10 +25,12 @@ var searchRecipies = function(data) {
       
     // make a request to the url
     fetch(apiUrl).then(function(response) {
+        console.log(response);
         response.json().then(function(data) {
         console.log(data);
         });
       });
     };
-    searchRecipies();
+   
+    userFormEl.addEventListener("submit", formSumbitHandler);
   
