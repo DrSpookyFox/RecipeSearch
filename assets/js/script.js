e8bd374adcb729639865c4c9e5a3de5162f257f8
Ed Kubiak <ek33450505@gmail.com>
2022-03-24 19:31:43 -0400

    corrected first call

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var recipeSearchTerm = document.querySelector("#recipe-container");
 //Global Variables
 var apiKey = "76fc45feadbe46379e4c23a107066a2f";
 
+
+
 var formSumbitHandler = function(event) {
     //Prevents browser from sending the form's input data to a URL
     event.preventDefault();
@@ -30,10 +32,10 @@ var searchRecipies = function(recipe) {
     fetch(apiUrl).then(function(response) {
        // request was successful
        if (response.ok) {
-        console.log(response);
+        // console.log(response);
         response.json().then(function(data) {
-          console.log(data);
-          displayRecipies(data, results);
+          // console.log(data.results);
+          recipeResults(data.results);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -44,18 +46,12 @@ var searchRecipies = function(recipe) {
     });
 };
 
-    var displayRecipies = function(results, searchTerm) {
-     //check if api returned any results
-     if (results.length === 0) {
-         recipeImgEl.textContent = "No recipies found.";
-         return;
+//display recipies on cards formatted in the HTML
+  var recipeResults = function(results) {
+    // console.log(results);
+    for (var i = 0; i < results.length; i++) {
+    console.log(results[i]);
     }
-
-        recipeSearchTerm.textContent = searchTerm;
-
-        for(var i = 0; i < results.length; i++) {
-            var recipeName = results[i].img + '/' + results[i].title;
-        }
-    };
+ };
 
     userFormEl.addEventListener("submit", formSumbitHandler);
