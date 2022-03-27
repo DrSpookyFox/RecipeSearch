38d5b264d452f20864d998b1379928580fc4f4d2
mwelsh356 <97926292+mwelsh356@users.noreply.github.com>
2022-03-27 19:02:06 -0400

    Fixed up CSS

--- assets/java/script.js
+++ assets/java/script.js
@@ -3,52 +3,66 @@ var nameInputEl = document.querySelector("#recipeSearch");
 
 //Global Variables
 var apiKey = "76fc45feadbe46379e4c23a107066a2f";
-var resultsArray = [];
-
 
 var formSumbitHandler = function(event) {
     //Prevents browser from sending the form's input data to a URL
     event.preventDefault();
-    // get  value from input element
+
+    //get value from input element
     var recipeSearch = nameInputEl.value.trim();
 
     if (recipeSearch) {
-        searchRecipies(recipeSearch);
-        nameInputEl.value = "";
-        } else 
-        alert("Please try again: not a valid search option!")
+      searchRecipies(recipeSearch);
+      nameInputEl.value = "";
+    } else {
+        alert("Invalid entry");
+    }
     }
 
 //function calls spooacular API to get recipe info
-var searchRecipies = function(data) {
+var searchRecipies = function(recipe) {
     // format the spoonacular api url
-    var apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=pizza`
+    var apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${recipe}&number=12`
       
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-        console.log(response);
+       // request was successful
+       if (response.ok) {
+        // console.log(response);
         response.json().then(function(data) {
-        displayQuery(data);
-        });
+          // console.log(data.results);
+          var htmlRecipeCards = recipeResults(data.results);
+          document.getElementById("cards-container").innerHTML = htmlRecipeCards;
+          var allButtons = document.querySelectorAll('.recipe-card');
+          console.log("Found", allButtons.length);
+    
+    for (var i = 0; i < allButtons.length; i++) {
+      allButtons[i].addEventListener('click', function() {
+      console.log(this.getAttribute("data-id"));
       });
-    };
-
-    var displayQuery = function(data) {
-        console.log(data);
-        console.log(recipeSearch);
-    };
-
-    var getRecipeInformation = function(data) {
-        // format the spoonacular api url
-        var apiUrl2 = `https://api.spoonacular.com/recipes/{id}/information?=apiKey=${apiKey}`
-
-        //make a request to the url
-        fetch(apiUrl2).then(function(response) {
-            console.log(response);
-            response.json().then(function(id) {
-                searchRecipies(id);
-            });
+    }
         });
-    };
-   
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      alert('Unable to connect to Spoonacular');
+    });
+};
+  
+//display recipies on cards formatted in the HTML
+  var recipeResults = function(results) { 
+    var htmlCards = "";
+    for (var i = 0; i < results.length; i++ ) {
+    htmlCards += `<div class="cell small-3 recipe-card" data-id="${results[i].id}"><div class="card">
+    <img src="${results[i].image}">
+    <div class="card-section">
+      <p>${results[i].title}</p>
+    </div>
+  </div></div>`
+    }
+    return htmlCards
+ };
+
     userFormEl.addEventListener("submit", formSumbitHandler);
