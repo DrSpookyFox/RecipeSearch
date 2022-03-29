f3b0c07ed7b97ec4103c95b5bb9a8eee60c6426b
Jarrett Hopkins <jarrettphopkins@Gmail.com>
2022-03-28 23:11:07 -0400

    test README file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # RecipeSearch
+
+## Our Goal
+The Recipe Search Website allows you to type in keywords and in return will desplay recipes that match the keywords used.  By clicking on one of the results, it tells you the ingrediants, stepts to make the recipe, and a link to the site with more specific information.
+
+[<img width="1592" alt="Screen Shot at 3/20/22" src="https://user-images.githubusercontent.com/98188411/160524266-ee297cdd-6b56-43c3-8509-72a97549d98c.JPG">]
+
+## Built Using
+CSS/HTML/JS/API
+
+## Visit our Website
+
+<!-- Need someone who has access to settings (Dr SPooky Fox) to go to settings and paste the git.io link below -->
+<!-- I do it by going to the setting of the respository and the going to the pages, you can make a link from there -->
+
+# This group project was completed by:
+ 
+* Jarrett Hopkins
+* Ed Kubiak
+* Hien Mai
+* Alexander Signer
+* Ibrahim Sylla
+* Michael Welsh
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,53 +2,101 @@ var userFormEl = document.querySelector("#search-form");
 var nameInputEl = document.querySelector("#recipeSearch");
 
 //Global Variables
+
 var apiKey = "76fc45feadbe46379e4c23a107066a2f";
-var resultsArray = [];
 
-
-var formSumbitHandler = function (event) {
+var formSumbitHandler = function(event) {
     //Prevents browser from sending the form's input data to a URL
     event.preventDefault();
-    // get  value from input element
+
+    //get value from input element
     var recipeSearch = nameInputEl.value.trim();
-
+    //check to see if user input matches search criteria
     if (recipeSearch) {
-        searchRecipies(recipeSearch);
-        nameInputEl.value = "";
-    } else
-        alert("Please try again: not a valid search option!")
-}
+      searchRecipies(recipeSearch);
+      nameInputEl.value = "";
+    } else {
+        alert("Invalid entry");
+    }
+    }
 
-//function calls spooacular API to get recipe info
-var searchRecipies = function (data) {
+//function calls spooacular API to get recipe title, img and id
+var searchRecipies = function(recipe) {
     // format the spoonacular api url
-    var apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=pizza`
-
+    var apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${recipe}&number=12`
+      
     // make a request to the url
-    fetch(apiUrl).then(function (response) {
-        console.log(response);
-        response.json().then(function (data) {
-            displayQuery(data);
+    fetch(apiUrl).then(function(response) {
+       // request was successful
+       if (response.ok) {
+        response.json().then(function(data) {
+          var htmlRecipeCards = recipeResults(data.results);
+          document.getElementById("cards-container").innerHTML = htmlRecipeCards;
+          var allButtons = document.querySelectorAll('.recipe-card');
+
+    for (var i = 0; i < allButtons.length; i++) {
+      allButtons[i].addEventListener('click', function() {
+      //need to trigger the modal to show
+      $('#ex1').show(); 
+      //Display the content in the body of the modal
+      });
+    }
         });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      alert('Unable to connect to Spoonacular');
     });
 };
 
-var displayQuery = function (data) {
-    console.log(data);
-    console.log(recipeSearch);
+ //create function to display selected recipe information
+ var selectedInfo = function(id) {
+  //format the spoonacular API url
+  var apiUrl2 = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
+
+  //make a request to the url
+  fetch(apiUrl2).then(function(response) {
+   if (response.ok) {
+     response.json().then(function(data) {
+       var ingredients = "";
+       for (var i = 0; i < data.extendedIngredients.length; i++ ) {
+        ingredients += (data.extendedIngredients[i].name  + ", ");
+       }
+      //  console.log(ingredients);
+      var htmlText = `<p> ${data.title} </p> 
+      <p> Ingredients: </p>
+      <p> ${ingredients} </p> 
+      <p> Instructions: </p>
+      <p> ${data.instructions}
+      <p> Source:  <a href="${data.sourceUrl}">${data.sourceUrl}</a></p>`;
+      
+      document.querySelector("#food-modal").innerHTML += htmlText;
+     });
+   }
+ })
 };
 
-var getRecipeInformation = function (data) {
-    // format the spoonacular api url
-    var apiUrl2 = `https://api.spoonacular.com/recipes/{id}/information?=apiKey=${apiKey}`
-
-    //make a request to the url
-    fetch(apiUrl2).then(function (response) {
-        console.log(response);
-        response.json().then(function (id) {
-            searchRecipies(id);
-        });
-    });
-};
-
+//display recipies on cards formatted in the HTML
+  var recipeResults = function(results) { 
+    var htmlCards = "";
+    for (var i = 0; i < results.length; i++ ) {
+    htmlCards += `<div class="cell small-3 recipe-card" onclick="selectedInfo(${results[i].id})" data-id="${results[i].id}"><div class="card">
+    <img src="${results[i].image}"<p><a href="#food-modal" rel="modal:open">Click For Details</a></p>
+    <div class="card-section">
+      <p>${results[i].title}</p>
+    </div>
+      <p></p>
+      </div> 
+  </div></div>`
+    }
+    return htmlCards
+ };
+  
 userFormEl.addEventListener("submit", formSumbitHandler);
+
+//we have a modal showing up after the grid-container / cards-container.
+//when we click an invididaul card they show up in sequence in a single modal, 
+//the formatting an information we want to display is all correct.
+//How do we get a new modal to display each time we click a card without other card info appearing  
